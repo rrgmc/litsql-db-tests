229bb609f2545990768964ab4a66f23822013cc2
Rangel Reale <rangelspam@gmail.com>
2024-06-04 20:25:49 -0300

    execute

---
+++ utils/execute.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/georgysavva/scany/v2/sqlscan"
+	"github.com/rrgmc/litsql/sq"
+	"gotest.tools/v3/assert"
+)
+
+func DBExecute(t *testing.T, db *sql.DB, query sq.BuildQuery, parseArgs map[string]any,
+	f func(row map[string]any)) {
+	squery, args, err := query.Build()
+	assert.NilError(t, err)
+
+	if parseArgs != nil {
+		args, err = sq.ParseArgs(args, map[string]any{
+			"length": 100,
+		})
+		assert.NilError(t, err)
+	}
+
+	rows, err := db.Query(squery, args...)
+	assert.NilError(t, err)
+	defer rows.Close()
+
+	srows := sqlscan.NewRowScanner(rows)
+	for rows.Next() {
+		r := map[string]any{}
+		err = srows.Scan(&r)
+		assert.NilError(t, err)
+		f(r)
+	}
+	assert.NilError(t, rows.Err())
+}
